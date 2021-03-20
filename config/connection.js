b2f4e7e7ccf046ad305b6ce4514c2f696cfba0af
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-03-19 22:13:17 -0500

    adding basic structure of repo

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// import sequalize 
+const sequalize = require('sequelize');
+
+require('dotenv').config();
+
+let sequalize;
+
+if (process.env.JAWSDB_URL) {
+    sequalize = new Sequalize(process.env.JAWSDB_URL);
+}else {
+    // connect to database
+    sequalize = new Sequalize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
+
+module.exports = sequalize;
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF EXISTS blob_site_db;
+
+CREATE DATABASE blog_site_db;
