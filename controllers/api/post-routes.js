c33e92b79d360a026c4ddf9e98f3bfadd14d0740
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-03-23 23:03:50 -0500

    added save and delete functionality. edited styling

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,135 +1,143 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
-const sequelize = require('../../config/connection');
+
+// our custom middleware
 const withAuth = require('../../utils/auth');
 
-// get all users
-router.get('/', (req, res) => {
-    console.log('======================');
+// GET api/posts
+router.get('/', (req,res) => {
     Post.findAll({
         attributes: [
-            'id',
-            'title',
-            'created_at',
-            'content'
+            'id', 
+            'content', 
+            'title', 
+            'created_at'
         ],
-      order: [['created_at', 'DESC']],
-      include: [
-        // Comment model here -- attached username to comment
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
-          model: User,
-          attributes: ['username']
-        },
-      ]
+        // display the most recently added posts first.
+        order: [['created_at', 'DESC']], 
+        include: [
+            // include the Comment model here:
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                model: User,
+                attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
     })
-      .then(dbPostData => res.json(dbPostData))
-      .catch(err => {
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
         console.log(err);
         res.status(500).json(err);
-      });
-  });
-
-  router.get('/:id', (req, res) => {
-    Post.findOne({
-      where: {
-        id: req.params.id
-      },
-      attributes: [
-        'id',
-        'title',
-        'created_at',
-        'content'
-      ],
-      include: [
-        // include the Comment model here:
-        {
-          model: User,
-          attributes: ['username']
-        },
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        }
-      ]
-    })
-      .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-        res.json(dbPostData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-
-router.post('/', withAuth, (req, res) => {
-    Post.create({
-      title: req.body.title,
-      content: req.body.content,
-      user_id: req.session.user_id
-    })
-      .then(dbPostData => res.json(dbPostData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+    });
 });
 
-router.put('/:id', withAuth, (req, res) => {
-    Post.update({
-        title: req.body.title,
-        content: req.body.content
-      },
-      {
+// GET api/posts/1
+router.get('/:id', (req, res) => {
+    Post.findOne({
         where: {
-          id: req.params.id
-        }
-      })
-      .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-        res.json(dbPostData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-
-  router.delete('/:id', withAuth, (req, res) => {
-    Post.destroy({
-      where: {
-        id: req.params.id
-      }
+                id: req.params.id
+        },
+        attributes: [
+            'id', 
+            'content', 
+            'title', 
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
     })
-      .then(dbPostData => {
+    .then(dbPostData => {
+        // test if there is a post with that id
         if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
         }
         res.json(dbPostData);
-      })
-      .catch(err => {
+    })
+    .catch(err => {
         console.log(err);
         res.status(500).json(err);
-      });
-  });
+    });
+});
 
-  module.exports = router;
+// POST api/posts/
+router.post('/', withAuth, (req, res) => {
+    Post.create({
+        title: req.body.title,
+        content: req.body.content,
+        // get user id from the session
+        user_id: req.session.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// PUT api/posts/1
+router.put('/:id', withAuth, (req,res) => {
+    Post.update( {
+            title: req.body.title,
+            content: req.body.content
+        },
+        {
+            where: {
+            id: req.params.id
+            }
+        }
+    )
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// DELETE api/posts/1
+router.delete('/:id', withAuth, (req, res) => {
+    Post.destroy({
+        where: {
+        id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        // see if there is a matching id
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,28 +1,28 @@
 async function editFormHandler(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[name="post-title"]').value;
-    const content = document.querySelector('input[name="content"]').value;
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-      ];
+  event.preventDefault();
 
-    const response = await fetch(`/api/posts/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            title,
-            content
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-      });
-      
-      if (response.ok) {
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_content = document.querySelector('input[name="post-content"]').value;
+  const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+
+  const response = await fetch(`/api/posts/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          title,
+          post_content
+      }),
+      headers: {
+          'Content-Type': 'application/json'
       }
-  }
-  
-  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+    });
+    
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
