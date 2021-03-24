55562a0b8a3bc915544ec4ff6d2d5fb8fa0f19c9
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-03-24 11:05:00 -0500

    add heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.SESSION_SECRET,
+    secret: 'secrets',
     cookie: {},
     resave: false,
     saveUninitialized: true,
