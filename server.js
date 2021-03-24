e95f5cd7dbdb14ceadf6d713eae2b8082b4402df
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-03-24 10:57:41 -0500

    add heroku

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(require('./controllers'));
 
 
 //turn on connection to db and server
-sequelize.sync({ force:false}).then(() => {
+sequelize.sync({ force:true}).then(() => {
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
